79dd510d126891b4355f05011b1d350c0ed87a55
heitorviana-dev <heitorsoaresviana@gmail.com>
2024-08-25 16:17:54 -0300

    Criando a base de dados e a tabela

--- server.js
+++ server.js
@@ -4,5 +4,6 @@ const app = require('./app');
 const port = process.env.PORT;
 
 app.listen(port, () => {
-  console.log('Servidor escutando na porta 3002 .');
+  console.log('Servidor escutando na porta 3002');
+  console.log('Acesse http://localhost:3002');
 });
