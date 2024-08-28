2759012fdd8c418a57a945b7ddde97e1d55966e1
heitorviana-dev <heitorsoaresviana@gmail.com>
2024-08-28 14:16:36 -0300

    Criando o input da tarefa

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,13 @@
 import { Fragment } from 'react';
 import './App.css';
 
+//Components
+import InputTodo from './components/InputTodo';
+
 function App() {
   return (
     <Fragment>
-      
+      <InputTodo/>
     </Fragment>
   );
 }
---
+++ client/src/components/InputTodo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const InputTodo = () => {
+  return(
+    <h1>
+      Hello World
+    </h1>
+  );
+}
+
+export default InputTodo;
